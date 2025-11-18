9a5786c7c4e1034dd2e83c5c6b04185af9e662bd
pKarpa <pavlo.karpa17@gmail.com>
2025-11-18 13:47:09 +0200

    user-say: desktop done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,53 @@
   const burgerBtn = document.querySelector("#btn-burger");
   const mobileContainer = document.querySelector("#burger-menu")
+  const prevUse = document.querySelector("#use-prev");
+  const nextUse = document.querySelector("#use-next");
+  const navNextBtn = document.querySelector("#nav-next-btn");
+  const navPrevBtn = document.querySelector("#nav-prev-btn");
+  
 
   burgerBtn.addEventListener("click", ()=>{
     burgerBtn.classList.toggle("active")
     mobileContainer.classList.toggle("show")
   });
+
+  const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 'auto',
+  speed: 800,
+  initialSlide: 1,
+  centeredSlides: true,
+  spaceBetween: 32,
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+navPrevBtn.addEventListener('mouseover', () => {
+  prevUse.setAttribute('href', '#arrow-white');
+  navPrevBtn.style.transform='rotate(180deg)'
+});
+
+navPrevBtn.addEventListener('mouseout', () => {
+  prevUse.setAttribute('href', '#arrow-black');
+  navPrevBtn.style.transform='rotate(360deg)'
+});
+
+navNextBtn.addEventListener('mouseover', () => {
+  nextUse.setAttribute('href', '#arrow-white');
+  navNextBtn.style.transform='rotate(360deg)'
+});
+
+navNextBtn.addEventListener('mouseout', () => {
+  nextUse.setAttribute('href', '#arrow-black');
+  navNextBtn.style.transform='rotate(180deg)';
+});
